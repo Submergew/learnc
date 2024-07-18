6bd20a81ad62289ce568af961417b7adb69a1c1e
submergew <wangziyu0412@outlook.com>
2024-07-18 15:55:55 +0800

    代码优化，重命名

---
+++ ch1/sizeof.c
@@ -0,0 +1,11 @@
+//
+// Created by wangz on 2024/7/18.
+//
+#include <stdio.h>
+int main() {
+  int a = 10;
+  printf("%d\n", sizeof(a));
+  printf("%d\n", sizeof(int));
+  printf("%d\n", sizeof a);
+  return 0;
+}
