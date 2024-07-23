9df6f70720b57ace74990fe75df98f609ee106a3
submergew <wangziyu0412@outlook.com>
2024-07-23 21:49:42 +0800

    宏

---
+++ chapter1/macros.c
@@ -0,0 +1,20 @@
+//
+// Created by wangz on 2024/7/23.
+//
+#include <stdio.h>
+
+#define PI 3.14
+#define EXP 10 * 1.5
+
+int main() {
+
+  float f;
+  f = 10 * PI;
+
+  float f2;
+  f2 = 10 * EXP;
+
+  printf("%f\n %f\n", f, f2);
+
+  return 0;
+}
