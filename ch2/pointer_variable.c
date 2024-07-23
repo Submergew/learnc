d8abafbf4ba839799d183ad82f6264309a46fe02
submergew <wangziyu0412@outlook.com>
2024-07-23 21:22:19 +0800

    指针

--- ch2/pointer_variable.c
+++ ch2/pointer_variable.c
@@ -4,8 +4,12 @@
 #include <stdio.h>
 int main() {
   int a = 10;
+  printf("%d\n", &a);
+
   int *p = &a;
   *p = 20;
-  printf("%d\n", a);
+  printf("%d\n", p);
+  printf("%d\n", *p); // 20
+  printf("%d\n", a);  // 20
   return 0;
 }
