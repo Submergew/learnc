cbe111dbbaf06abecc83dddc23f9befcfdd32b4c
submergew <wangziyu0412@outlook.com>
2024-07-24 21:39:24 +0800

    数组指针

---
+++ chapter1/pointer_array.c
@@ -0,0 +1,20 @@
+//
+// Created by wangz on 2024/7/24.
+//
+#include <stdio.h>
+int main() {
+  int a[4];
+  int i;
+  for (i = 0; i < 4; i++)
+    ;
+  {
+    scanf("%d", &a[i]);
+    getchar();
+  }
+  for (i = 0; i < 4; i++)
+    printf("%p\n", &a[i]);
+  for (i = 0; i < 4; i++)
+    printf("%d\n", *(a + i));
+
+  return 0;
+}
