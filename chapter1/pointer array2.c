730827083ac096f1f026fa706be7ef80fec8de4d
submergew <wangziyu0412@outlook.com>
2024-07-25 11:51:50 +0800

    指针数组

---
+++ chapter1/pointer array2.c
@@ -0,0 +1,13 @@
+//
+// Created by wangz on 2024/7/24.
+//
+#include <stdio.h>
+int main() {
+  int b[4] = {10, 20, 30, 40};
+  int a[4];
+
+  a[4] = 200;
+  printf("b[0]:%d\n", b[0]);
+
+  return 0;
+}
