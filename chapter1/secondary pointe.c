6e24c00d93e65f0a1f0804233413958fee56d691
submergew <wangziyu0412@outlook.com>
2024-07-25 13:59:12 +0800

    二级指针

--- chapter1/secondary pointe.c
+++ chapter1/secondary pointe.c
@@ -7,8 +7,12 @@ int main() {
   int a = 10;
   int *p = &a;
   int **p2 = &p;
-
+  int ***p3 = &p2;
   printf("%p %p %P\n", *p2, p, &a);
   printf("%d %d %d %d\n", **p2, *p, *(&a));
+
+  **p2 = 100;
+  printf("%d %d %d %d\n", **p2, *p, *(&a), a);
+  printf("***p3:%d\n", ***p3);
   return 0;
 }
