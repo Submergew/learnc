833a28f139213fb3b9dee1b911a27abf205fe4ba
submergew <wangziyu0412@outlook.com>
2024-07-25 21:47:47 +0800

    realloc重新分配内存

---
+++ chapter9/realloc.c
@@ -0,0 +1,20 @@
+
+//
+// Created by wangz on 2024/7/25.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+  char *tmp;
+  tmp = (char *)malloc(10);
+  strcpy(tmp, "hellow");
+
+  printf("%p\n", tmp);
+  tmp = realloc(tmp, 10 + 10);
+  printf("%p\n", tmp);
+  printf("%s\n", tmp);
+
+  strcat(tmp, "world");
+  return 0;
+}
