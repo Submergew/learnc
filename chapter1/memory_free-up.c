b142dcc5c016d4938e2266f3e43fb3d0b0cd728d
submergew <wangziyu0412@outlook.com>
2024-07-25 21:14:34 +0800

    free内存释放

---
+++ chapter1/memory_free-up.c
@@ -0,0 +1,23 @@
+//
+// Created by wangz on 2024/7/25.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void func() {
+  char *tmp;
+  tmp = (char *)malloc(20);
+}
+
+int main() {
+  char *tmp;
+  tmp = (char *)malloc(20);
+  strcpy(tmp, "hellow");
+  printf("%s\n", tmp);
+  printf("%p\n", tmp);
+  free(tmp); // 释放堆上的空间,tmp依然指向原来的堆空间
+  printf("%s\n", tmp); //
+  printf("%p\n", tmp);
+  return 0;
+}
