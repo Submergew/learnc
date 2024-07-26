5f2d6d82cfcc7a03c327c7a28ea4d80a0d3c10ed
submergew <wangziyu0412@outlook.com>
2024-07-26 18:41:49 +0800

    atoi和atof函数

---
+++ chapter9/atoi_atof.c
@@ -0,0 +1,15 @@
+
+//
+// Created by wangz on 2024/7/26.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+
+  printf("%d\n", atoi("30"));
+  printf("%d\n", atoi("30x"));
+
+  printf("%f\n", atof("28.5"));
+  return 0;
+}
