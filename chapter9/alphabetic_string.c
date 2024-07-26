e184e94e499baaf818feb69d975fbb4a768153e0
submergew <wangziyu0412@outlook.com>
2024-07-26 17:57:15 +0800

    指针内存

---
+++ chapter9/alphabetic_string.c
@@ -0,0 +1,16 @@
+
+//
+// Created by wangz on 2024/7/25.
+//
+#include <stdio.h>
+int main() {
+
+  char *p = "hello";
+  int i;
+  for (i = 0; i < 6; i++)
+    printf("%d ", p[i]); //*(p+i)
+  printf("\n");
+  printf("%d\n", sizeof("hello"));
+
+  return 0;
+}
