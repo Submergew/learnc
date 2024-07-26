dad29ace55e2fd169f81b1d0eb88b5270e6ea755
submergew <wangziyu0412@outlook.com>
2024-07-26 21:35:41 +0800

    strcpy拷贝

---
+++ chapter9/strcpy2.c
@@ -0,0 +1,17 @@
+//
+// Created by wangz on 2024/7/26.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+  char a[4];
+  char b[4];
+
+  strcpy(b, "hello");
+
+  printf("b: %s\n", b);
+  printf("a: %s\n", a);
+
+  return 0;
+}
