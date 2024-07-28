464b63a2c170690e1aa1eb7d38132170ce4a123d
submergew <wangziyu0412@outlook.com>
2024-07-28 12:11:38 +0800

    strcat函数

--- chapter9/strcat.c
+++ chapter9/strcat.c
@@ -1,3 +1,23 @@
-//
-// Created by wangz on 2024/7/28.
-//
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  char dst[20];
+  memset(dst,0,sizeof(dst));
+  char *src = "hello";
+
+  strcpy(dst,"world");
+  strcat(dst,src);
+  printf("dst:%s\n",dst);
+
+  char dst1[20];
+  memset(dst1,0,sizeof(dst));
+  char *src1 = "he\0llo";
+
+  strcpy(dst1,"world");
+  strcat(dst1,src1);
+  printf("dst1:%s\n",dst1);
+
+
+  return 0;
+}
