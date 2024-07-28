fd2f420f6e7566509e45bda5187f01d3ec31c1e2
submergew <wangziyu0412@outlook.com>
2024-07-28 18:58:16 +0800

    struct结构体

--- chapter10/struct.c
+++ chapter10/struct.c
@@ -3,9 +3,12 @@
 struct student {
   char name[32];
   int age;
+  char gender;
+  float score;
 };
 int main() {
-  struct student s;
+  struct student s = {"zhangsan",18,'m',99.5};
+  struct student s2 = {.age=18, .name="zhangsan", .score=99.5, .gender='m'};
   struct student *p;
   struct student a[10];
   return 0;
